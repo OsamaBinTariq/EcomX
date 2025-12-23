735958322ac6c732f37d725078fe337683d67495
OsamaBinTariq <osamagdboy@gmail.com>
2025-12-23 16:28:13 +0500

    Add Context API

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom' // Import this
+import { BrowserRouter } from 'react-router-dom'
+import { CartProvider } from './context/CartContext.jsx' 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter> {/* Wrap App with this */}
-      <App />
+    <BrowserRouter>
+      <CartProvider> 
+        <App />
+      </CartProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
