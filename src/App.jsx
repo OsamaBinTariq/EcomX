9ed956ac0beb2a4747e8ce7582215360b230ea9d
OsamaBinTariq <osamagdboy@gmail.com>
2025-12-29 15:32:16 +0500

    Add Analytics
    vercel dashboard analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,22 @@ import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
-import ProductDetails from './pages/ProductDetails'; 
+import ProductDetails from './pages/ProductDetails';
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <>
       <Navbar />
+      
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/product/:id" element={<ProductDetails />} />
       </Routes>
+
+     
+      <Analytics />
     </>
   );
 }
